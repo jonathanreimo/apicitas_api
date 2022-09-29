e726b4ea1fd8104804eecfad5978fe4ecccbdde1
jonathanreimo <jon.mauricio@outlook.com>
2022-09-29 13:40:02 -0500

    ajuste de conexion

--- conexion.php
+++ conexion.php
@@ -5,8 +5,8 @@ class Conexion {
  public function getConexion(){
    $host = "127.0.0.1";  //127.0.0.1 0 localhost
    $db = "backend_inertia";      //base de datos de mysql
-   $user = "sail";       // usuario de mysql
-   $password = "password";       //contraseña de mysql
+   $user = "lvdbu";       // usuario de mysql
+   $password = "sunshine8289";       //contraseña de mysql
 
 //conexion a la base datos utilizando pdo
  $db = new PDO("mysql:host=$host;dbname=$db;", $user, $password);
