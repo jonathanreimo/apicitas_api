dabb24d5ee0f78b22240eacf45015bbee9453ec2
jonathanreimo <jon.mauricio@outlook.com>
2022-10-06 12:43:49 -0500

    segunda ronda api

---
+++ DBConfig.php
@@ -0,0 +1,15 @@
+<?php
+ 
+//Define your host here.
+$HostName = "localhost";
+ 
+//Define your database name here.
+$DatabaseName = "apiss";
+ 
+//Define your database username here.
+$HostUser = "ziodbu";
+ 
+//Define your database password here.
+$HostPass = "25012020";
+ 
+?>
---
+++ submit_user_info.php
@@ -0,0 +1,60 @@
+<?php
+ 
+// Importing DBConfig.php file.
+include 'DBConfig.php';
+ 
+// Creating connection.
+ $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+ 
+ // Getting the received JSON into $json variable.
+ $json = file_get_contents('php://input');
+ 
+ // decoding the received JSON and store into $obj variable.
+ $obj = json_decode($json,true);
+ 
+ // Populate name from JSON $obj array and store into $name.
+$name = $obj['name'];
+ 
+// Populate email from JSON $obj array and store into $email.
+$email = $obj['email'];
+ 
+// Populate phone number from JSON $obj array and store into $phone_number.
+$slot = $obj['slot'];
+ 
+ // Creating SQL query and insert the record into MySQL database table.
+$Sql_Query = "INSERT INTO citas (name, email, slot) values ('$name','$email','$slot')";
+
+
+$sql = "SELECT * FROM slots WHERE slot='$slot'";
+ 
+$result = $con->query($sql);
+ 
+ while($row[] = $result->fetch_assoc()) {
+ 
+ //$tem = $row;
+ $idsl = $row['id'];
+ 
+ }
+
+$Sql_Query2 = "DELETE FROM slots WHERE id='$idsl'";
+ 
+ 
+ if(mysqli_query($con,$Sql_Query,$Sql_Query2,$sql)){
+ 
+ // If the record inserted successfully then show the message.
+$MSG = 'Data Inserted Successfully into MySQL Database' ;
+ 
+// Converting the message into JSON format.
+$json = json_encode($MSG);
+ 
+// Echo the message.
+ echo $json ;
+ 
+ }
+ else{
+ 
+ echo 'Try Again';
+ 
+ }
+ mysqli_close($con);
+?>
