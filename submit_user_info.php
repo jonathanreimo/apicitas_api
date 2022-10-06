3e5a118a12c08ce66843be8ca4f96689b516da56
jonathanreimo <jon.mauricio@outlook.com>
2022-10-06 18:02:25 -0500

    querys

--- submit_user_info.php
+++ submit_user_info.php
@@ -7,25 +7,25 @@ include 'DBConfig.php';
  $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
  
  // Getting the received JSON into $json variable.
- $json = file_get_contents('php://input');
- 
+ $json = json_decode(file_get_contents('php://input') , true);
  // decoding the received JSON and store into $obj variable.
- $obj = json_decode($json,true);
+ //$obj = json_decode($json,true);
  
  // Populate name from JSON $obj array and store into $name.
-$name = $obj['name'];
+//$name = $obj['name'];
  
 // Populate email from JSON $obj array and store into $email.
-$email = $obj['email'];
+//$email = $obj['email'];
  
 // Populate phone number from JSON $obj array and store into $phone_number.
-$slot = $obj['slot'];
+//$slot = $obj['slot'];
  
+$query = "INSERT INTO citas (name, email, slot) values ('$json[name]', '$json[email]', '$json[slot]')";
  // Creating SQL query and insert the record into MySQL database table.
-$Sql_Query = "INSERT INTO citas (name, email, slot) values ('$name','$email','$slot')";
+//$Sql_Query = "INSERT INTO citas (name, email, slot) values ('$name','$email','$slot')";
 
 
-$sql = "SELECT * FROM slots WHERE slot='$slot'";
+$sql = "SELECT * FROM slots WHERE slot='$json[slot]'";
  
 $result = $con->query($sql);
  
@@ -39,9 +39,9 @@ $result = $con->query($sql);
 $Sql_Query2 = "DELETE FROM slots WHERE id='$idsl'";
  
  
- if(mysqli_query($con,$Sql_Query,$Sql_Query2,$sql)){
+ if(mysqli_query($con, $sql, $query)){
  
-    return '{"msg":"usuario agregado"}';
+    $message = 'Success!';
  // If the record inserted successfully then show the message.
 //$MSG = 'Data Inserted Successfully into MySQL Database' ;
  
@@ -54,8 +54,9 @@ $Sql_Query2 = "DELETE FROM slots WHERE id='$idsl'";
  }
  else{
  
- echo 'Try Again';
+    $message = 'Error! Try Again.';
  
  }
+ echo json_encode($message);
  mysqli_close($con);
 ?>
