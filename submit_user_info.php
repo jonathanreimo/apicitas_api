9a51ac5c380ab675c18e5478df7d7e4f675d9f87
jonathanreimo <jon.mauricio@outlook.com>
2022-10-07 16:58:46 -0500

    querys

--- submit_user_info.php
+++ submit_user_info.php
@@ -11,13 +11,13 @@ header("Allow: POST");
 include 'DBConfig.php';
 
 $message = '';
-$connection = new mysqli($host_name, $host_user, $host_password, $database_name);
+$connection = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);
 if ($connection->connect_error)
 {
     die("Connection failed: " . $connection->connect_error);
 }
 $json = json_decode(file_get_contents('php://input') , true);
-$query = "INSERT INTO citas(name, email) values('$json[name]', '$json[email]')";
+$query = "INSERT INTO citas (name, email) values('$json[name]', '$json[email]')";
 $query_result = $connection->query($query);
 if ($query_result === true)
 {
