80e0950440c7547aaf72beff82a69a3d2746dc63
jonathanreimo <jon.mauricio@outlook.com>
2022-10-08 13:49:29 -0500

    querys

--- submit_user_info.php
+++ submit_user_info.php
@@ -19,8 +19,17 @@ if ($connection->connect_error)
 {
     die("Connection failed: " . $connection->connect_error);
 }
-$json = json_decode(file_get_contents('php://input') , true);
-$query = "INSERT INTO citas (name, email) values('$json[name]', '$json[email]')";
+//$json = json_decode(file_get_contents('php://input') , true);
+
+$encodedData = file_get_contents('php://input');  // take data from react native fetch API
+$decodedData = json_decode($encodedData, true);
+
+$UserName = $decodedData['name'];
+$UserEmail = $decodedData['email'];
+//$UserPW = md5($decodedData['Password']); 
+
+//$query = "INSERT INTO citas (name, email) values('$json[name]', '$json[email]')";
+$query = "INSERT INTO citas (name, email) values('$UserName', '$UserEmail')";
 $query_result = $connection->query($query);
 if ($query_result === true)
 {
@@ -30,6 +39,10 @@ else
 {
     $message = 'Error! Try Again.';
 }
-echo json_encode($message);
+$res[] = array("message" => $message);
+
+echo json_encode($res);
+
+//echo json_encode($message);
 }
 ?>
