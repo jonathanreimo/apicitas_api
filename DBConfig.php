537613c6bc5000d2540814f53b78739645953f1b
jonathanreimo <jon.mauricio@outlook.com>
2022-10-10 11:53:36 -0500

    querys

--- DBConfig.php
+++ DBConfig.php
@@ -1,15 +1,31 @@
 <?php
+
+$host='localhost';
+ $username='ziobdu';
+ $password='25012020'; 
+ $db_name='apiss'; 
+
+ $connect_db = mysqli_connect($host, $username, $password, $db_name);
+
+  
+ $React_APP_Data = file_get_contents('php://input'); 
+ $Decode_React_APP_Data = json_decode($React_APP_Data , true);
+
+if(!$connect_db)
+{
+	echo json_encode("Connection to DB Failed");
+}
  
 //Define your host here.
-$HostName = "localhost";
+//$HostName = "localhost";
  
 //Define your database name here.
-$DatabaseName = "apiss";
+//$DatabaseName = "apiss";
  
 //Define your database username here.
-$HostUser = "ziodbu";
+//$HostUser = "ziodbu";
  
 //Define your database password here.
-$HostPass = "25012020";
+//$HostPass = "25012020";
  
 ?>
--- submit_user_info.php
+++ submit_user_info.php
@@ -10,39 +10,41 @@ header("Allow: POST");
 // Importing DBConfig.php file.
 include 'DBConfig.php';
 
-$method = $_SERVER['REQUEST_METHOD'];
 
-if($method == "POST"){
-$message = '';
-$connection = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);
-if ($connection->connect_error)
+$json = json_decode(file_get_contents('php://input'), true);
+
+
+
+$First_Name = $Decode_React_APP_Data['First_Name'];
+//$Last_Name = $Decode_React_APP_Data['Last_Name'];
+$Email = $Decode_React_APP_Data['Email'];
+//$Password = ($Decode_React_APP_Data['Password']); //password is un-hashed mean with no hacking capability(change to md5 for secure pass)
+
+
+$query = "SELECT * FROM registrationapp_react WHERE Email = '$Email'";
+$query_result = mysqli_query($connect_db, $query);
+
+if (!mysqli_num_rows($query_result))
 {
-    die("Connection failed: " . $connection->connect_error);
-}
-//$json = json_decode(file_get_contents('php://input') , true);
+    $Reg_Query = "INSERT INTO citas(`nombre`, `email`) VALUES ('$First_Name', '$Email')";
+    $Reg_Query_Result = mysqli_query($connect_db, $Reg_Query);
 
-$encodedData = file_get_contents('php://input');  // take data from react native fetch API
-$decodedData = json_decode($encodedData, true);
-
-$UserName = $decodedData['name'];
-$UserEmail = $decodedData['email'];
-//$UserPW = md5($decodedData['Password']); 
-
-//$query = "INSERT INTO citas (name, email) values('$json[name]', '$json[email]')";
-$query = "INSERT INTO citas (name, email) values('$UserName', '$UserEmail')";
-$query_result = $connection->query($query);
-if ($query_result === true)
+    if ($Reg_Query_Result) 
+	{
+        $Message = "Registered successfuly!";
+    } else 
+	{
+        $Message = "Error - Try again";
+    }
+	
+} else 
 {
-    $message = 'Success!';
+    $Message = "User Already Registered";
 }
-else
-{
-    $message = 'Error! Try Again.';
-}
-$res[] = array("message" => $message);
 
-echo json_encode($res);
+$response[] = array("Message" => $Message);
 
-//echo json_encode($message);
-}
+echo json_encode($response);
+
+
 ?>
