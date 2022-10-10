a54e760412e4555cfc03137e559c256cb01b2516
jonathanreimo <jon.mauricio@outlook.com>
2022-10-10 12:50:22 -0500

    querys

--- submit_user_info.php
+++ submit_user_info.php
@@ -8,7 +8,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Allow: POST");
  
 // Importing DBConfig.php file.
-include 'DBConfig.php';
+include ('DBConfig.php');
 
 
 //$json = json_decode(file_get_contents('php://input'), true);
@@ -21,7 +21,7 @@ $Email = $Decode_React_APP_Data['Email'];
 //$Password = ($Decode_React_APP_Data['Password']); //password is un-hashed mean with no hacking capability(change to md5 for secure pass)
 
 
-$query = "SELECT * FROM registrationapp_react WHERE Email = '$Email'";
+$query = "SELECT * FROM citas WHERE email = '$Email'";
 $query_result = mysqli_query($connect_db, $query);
 
 if (!mysqli_num_rows($query_result))
