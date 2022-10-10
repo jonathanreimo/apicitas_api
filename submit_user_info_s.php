0fa48fdbbd35551f12b74461ad17d256fa8aec50
jonathanreimo <jon.mauricio@outlook.com>
2022-10-10 17:48:19 -0500

    querys

--- submit_user_info_s.php
+++ submit_user_info_s.php
@@ -1,11 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: x-api-key, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-// Encabezado de contenido de acuerdo a lo que se va a devolver
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, Authorization,X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Content-type: application/json; charset=utf-8");
-// Permite solo el método POST para evitar errores
-header("Access-Control-Allow-Methods: POST");
-header("Allow: POST");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
  
 // Importing DBConfig.php file.
 include 'DBConfig.php';
