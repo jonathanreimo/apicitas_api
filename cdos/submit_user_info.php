df934e1bf8e0d9d7b121d7d823cc6c772a7c0ade
jonathanreimo <jon.mauricio@outlook.com>
2022-10-11 11:38:40 -0500

    querys

--- cdos/submit_user_info.php
+++ cdos/submit_user_info.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, Authorization,X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Content-type: application/json; charset=utf-8");
+header("Content-type: application/json");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
  
