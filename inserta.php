228ab9a22ea6faa0753922048337fa9f9ece46fd
jonathanreimo <jon.mauricio@outlook.com>
2022-10-15 13:23:09 -0500

    querys

--- inserta.php
+++ inserta.php
@@ -15,8 +15,7 @@
     $m_name = $DecodedData['m_name'];
     $m_course = $DecodedData['PickerValueHolder'];
 
-
-    $insertMemberData = "DELETE FROM slots WHERE slot='$m_course'";
+    $insertMemberData = "INSERT INTO citas (name, email, slot) values ('$m_no', '$m_name', '$m_course')";
 
     $register = mysqli_query($CN, $insertMemberData);
 
